2d8d75f581578e186577182a119c5afae41ddfbf
shachar helmer himself <shachar.helmer@gmail.com>
2020-03-07 18:17:20 +0200

    __pycache__ not tracked. Stored procedures being tracked by post-commit srcipt for mysql, while also being delimited so they could be re-run to be re-created no matter what the previous delimiter was

--- post-commit.py
+++ post-commit.py
@@ -13,17 +13,26 @@ def get_mysql_create_procedure_query(engine, sp_name):
     res = engine.execute(Template(MYSQL_SHOW_CREATE_PROCEDURE_TEMPLATE).substitute(spname=sp_name))
     return [value for row in res for column, value in row.items() if column == 'Create Procedure'][0]
 
+def mysqldump_command_setup(settings):
+    command = MYSQL_SCHEMA_TEMPLATE
+    if settings['tables']['no_drop_tables']:
+        command += MYSQL_NO_DROP_FLAG
+    return command
+
 def main():
-    db_settings = load_conf()['db_settings']
-    eng = create_engine(db_settings['conn_string'])
-    # mysql dump output
-    system(Template(MYSQL_SCHEMA_TEMPLATE).substitute(dbname=db_settings['db'],
-                                                      user=db_settings['user']))
-    sps = get_mysql_procedure_names(engine=eng, db_name=db_settings['db'])
+    settings = load_conf()
+    eng = create_engine(settings['db']['conn_string'])
+    # mysqldump output
+    system(Template(mysqldump_command_setup(settings)).substitute(dbname=settings['db']['db_name'],
+                                                      user=settings['db']['user']))
+    # write stored procedures create queries
+    sps = get_mysql_procedure_names(engine=eng, db_name=settings['db']['db_name'])
     create_sp_queries = [get_mysql_create_procedure_query(engine=eng, sp_name=sp) for sp in sps]
-    with open(f'stored_procedures-{db_settings["db"]}', 'w'):
+    with open('stored_procedures-{0}'.format(settings['db']['db_name']), 'w') as sp_file:
+        sp_file.write('DELIMITER //' + '\n')
         for create_sp_query in create_sp_queries:
-            pass
+            sp_file.write(create_sp_query + '//\n')
+        sp_file.write('DELIMITER ;' + '\n')
 
 
 if __name__ == "__main__":
--- templates.py
+++ templates.py
@@ -1,3 +1,4 @@
 MYSQL_SCHEMA_TEMPLATE = 'mysqldump -u $user -d $dbname > $dbname.sql'
+MYSQL_NO_DROP_FLAG = ' --skip-add-drop-table'
 MYSQL_PROCEDURE_NAMES = 'SHOW PROCEDURE STATUS WHERE db = \'$dbname\''
 MYSQL_SHOW_CREATE_PROCEDURE_TEMPLATE = 'SHOW CREATE PROCEDURE $spname'
